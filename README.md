6530e93351789e42318909cfabd85ef67cd01a23
Yevhenii <polotnyak2004@gmail.com>
2024-07-23 02:34:14 +0300

    clone museum landing

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
-# Base layout template with Gulp, SCSS and Stylelint
-1. Create a repo using this template
-1. Replace `<your_account>` and `<repo_name>` with your Github username and the new repo name
-    - [DEMO LINK](https://<your_account>.github.io/<repo_name>/)
+# Museum Landing Page
+
+## Description
+This project is a landing page implementation based on the [Figma design](https://www.figma.com/design/cRBCqE06cDrY3s4jX7h3iY/%D0%9D%D0%90%D0%9C%D0%A3-(Edit)?node-id=0-1&t=XDNd9yMkLf5EkH4d-0).
+It uses BEM and SCSS for styling and adapts to various screen sizes.
+
+## Technologies Used
+- HTML
+- CSS (SCSS)
+- JavaScript
+- BEM Methodology
+- Figma for design
+
+## Links
+- **Preview:** [DEMO LINK](https://yepolotn1ak.github.io/museum-landing/)
+- **Mockup:** [Figma Design](https://www.figma.com/design/cRBCqE06cDrY3s4jX7h3iY/%D0%9D%D0%90%D0%9C%D0%A3-(Edit)?node-id=0-1&t=XDNd9yMkLf5EkH4d-0)
+
+## Launch Instructions
+1. **Fork** the repo.
+2. **Clone** the forked repo.
+3. Run `npm install` (or `npm i`).
+4. Run `npm start`.
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,84 @@
 'use strict';
+
+const slider = document.querySelector('.galary__slider--img-container');
+const slide = document.querySelector('.galary__slider--photo');
+const buttons = document.querySelector('.galary__slider--buttons');
+
+let currentIndex = 0;
+const sliderData = [];
+let [slideWidth, translateValue, slidesOfScreen, gapValue, visibleButtons]
+  = sliderData;
+
+const getTranslate = (index, slidesCount) => {
+  let translateXValue;
+
+  if (slidesCount === 1 && index === 3) {
+    translateXValue = index * (slideWidth + gapValue) - translateValue;
+  } else if ((slidesCount === 2 && index >= 2)
+    || (slidesCount === 3 && index >= 1)
+  ) {
+    translateXValue = 3 * (slideWidth + gapValue) - translateValue;
+  } else {
+    translateXValue = index * (slideWidth + gapValue);
+  }
+
+  slider.style.transform = `translateX(-${translateXValue}px)`;
+};
+
+const getSliderButtons = (slidesCount, buttonsCount) => {
+  switch (slidesCount) {
+    case 1:
+      [...buttons.children].forEach(btn => (btn.style.display = 'block'));
+      break;
+    case 2:
+      if (buttonsCount < 3) {
+        buttons.lastElementChild.style.display = 'block';
+      } else if (buttonsCount > 3) {
+        buttons.lastElementChild.style.display = 'none';
+      }
+      break;
+    case 3:
+      if (buttonsCount !== 2) {
+        [...buttons.children]
+          .slice(-2).forEach(btn => (btn.style.display = 'none'));
+      }
+      break;
+  }
+};
+
+const resize = () => {
+  const buttonsCount = [...buttons.children].reduce((count, button) => {
+    return window.getComputedStyle(button).display === 'block'
+      ? count + 1
+      : count;
+  }, 0);
+
+  if (slider && slide) {
+    sliderData.splice(
+      0,
+      5,
+      slide.clientWidth,
+      slider.clientWidth - slide.clientWidth,
+      Math.floor(slider.clientWidth / slide.clientWidth),
+      parseInt(window.getComputedStyle(slider).gap),
+      buttonsCount,
+    );
+
+    [slideWidth, translateValue, slidesOfScreen, gapValue, visibleButtons]
+      = sliderData;
+    getTranslate(currentIndex, slidesOfScreen);
+    getSliderButtons(slidesOfScreen, visibleButtons);
+  }
+};
+
+resize();
+
+[...buttons.children].forEach((button, index) => {
+  button.addEventListener('click', () => {
+    slider.style.transition = 'transform .5s';
+    currentIndex = index;
+    getTranslate(index, slidesOfScreen);
+  });
+});
+
+window.addEventListener('resize', resize);
